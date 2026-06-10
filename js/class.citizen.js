07aab61b0f9b224c00604ef259324f5577a312dc
Jannick-L <jannick.la05@gmail.com>
2026-06-10 19:23:19 +0200

    Completed Assignment

--- js/class.citizen.js
+++ js/class.citizen.js
@@ -13,8 +13,13 @@
 
 export default class Citizen{
 
-    /* Math.random() gives a value from 0 to <1
-    3 scales it to 0–<3
-    Math.floor(...) makes it 0, 1, 2
-    + 2 shifts it to 2, 3, 4 */
+    constructor(name) {
+        this.name = name;
+        this.rank = Math.floor((Math.random() * 3) + 2);
+        this.home = null;
+    }
+
+    toString() {
+        return `${this.name}, ${this.rank}`;
+    }
 }
